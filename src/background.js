c8d85a7b2f25dfa86fdd5ff078a8ed29c6d37304
rosulek <rosulekm@eecs.oregonstate.edu>
2015-11-30 16:52:32 -0800

    factor clipping function to background.js; beginnings of config page

--- src/background.js
+++ src/background.js
@@ -4,12 +4,27 @@
 
 
 function g(){return((1+Math.random())*65536|0).toString(16).substring(1)}
-localStorage.pollid = g() + g();
+function generateUUID(){return g()+g()+"-"+g()+"-"+g()+"-"+g()+"-"+g()+g()+g()}
 
-console.log("Initializing with localstorage = ", localStorage);
+function debug() {
+    if (localStorage.debug)
+        console.log.apply(console,arguments);
+}
+
+chrome.contextMenus.create({
+    "title":    "Configure",
+    "contexts": ["browser_action"],
+    "onclick":  function() {
+        chrome.tabs.create({ url: chrome.extension.getURL("config.html") });
+    }
+});
+
+
+localStorage.pollid = g() + g();
+debug("Initializing with localstorage = ", localStorage);
 
 function refreshWorkflowyData(callback) {
-    console.log("refreshing workflowy data...");
+    debug("refreshing workflowy data...");
     var xhr = new XMLHttpRequest();
     xhr.open('GET', 'https://workflowy.com/get_initialization_data?client_version=15');
     xhr.onload = function () { 
@@ -25,17 +40,111 @@ function refreshWorkflowyData(callback) {
             localStorage.transid = obj.projectTreeData.mainProjectTreeInfo.initialMostRecentOperationTransactionId;
             delete localStorage["inbox"];
 
-            console.log("succesfully got initialization data:", localStorage);
+            debug("succesfully got initialization data:", localStorage);
             callback(0);
 
         } catch (e) {
             localStorage.clear();
-            console.log("error extracting initialization data", e);
+            debug("error extracting initialization data", e);
             callback(1);
         }
 
     };
     xhr.send();
-    console.log("sending XHR");
+    debug("sending XHR");
 }
+
+function htmlEsc(str) {
+    return str.replace(/&/g,"&amp;").replace(/>/g,"&gt;").replace(/</g,"&lt;");
+}
+
+function clipToWorkflowy(title, comment, callback) {
+    var newuuid = generateUUID();
+    var timestamp = Math.floor((new Date()).getTime()/1000) - localStorage.joined;
+    
+    var request = [{
+        "most_recent_operation_transaction_id": localStorage.transid,
+        "operations": [
+            { "type": "create",
+              "data": {
+                  "projectid": newuuid,
+                  "parentid":  localStorage.inbox,
+                  "priority":  9999
+               },
+              "client_timestamp": timestamp,
+              "undo_data":        {}
+            },
+            { "type":"edit",
+              "data": {
+                  "projectid":   newuuid,
+                  "name":        htmlEsc(title),
+                  "description": htmlEsc(comment)
+              },
+              "client_timestamp": timestamp,
+              "undo_data": {
+                   "previous_last_modified": timestamp,
+                   "previous_name":          "",
+                   "previous_description":   ""
+              }
+            }
+        ]
+    }];
+
+    debug("Trying to clip something to workflowy:",
+        { timestamp: timestamp,
+          title: title,
+          comment: comment,
+          escapedTitle: htmlEsc(title),
+          escapedComment: htmlEsc(comment),
+          localstorage: localStorage }
+    );
+
+    var postdata = {
+        "client_id":          "2015-11-17 19:25:15.397732",
+        "client_version":     15,
+        "push_poll_id":       localStorage.pollid,
+        "push_poll_data":     JSON.stringify(request),
+        "crosscheck_user_id": localStorage.userid,
+    };
+
+    var urlEncData = "";
+    var sep = "";
+    for (var key in postdata) {
+        urlEncData += sep + encodeURIComponent(key) + "=" + encodeURIComponent(postdata[key]);
+        sep = "&";
+    }
+    
+    var xhr = new XMLHttpRequest();
+    xhr.open('POST', 'https://workflowy.com/push_and_poll', true);
+    xhr.setRequestHeader('Content-Type', 'application/x-www-form-urlencoded');
+    xhr.onload = function () {
+        try {
+            err = "parsing JSON";
+            respObj = JSON.parse(this.responseText);
+            debug("XHR response from server:", respObj);
+        } catch (e) {
+            debug("error processing XHR response: ", e);
+            callback(newuuid, "parsing JSON");
+            return;
+        }
+
+        if (respObj.logged_out) {
+            callback(newuuid, "logged out");
+        } else if ( ! respObj.results ) {
+            callback(newuuid, "no results");
+        } else if (respObj.results[0].error) {
+            callback(newuuid, "generic: " + respObj.results[0].error);
+//        } else if (respObj.results[0].error_encountered_in_remote_operations) {
+//            callback(newuuid, "unknown: " + respObj.results[0].error_encountered_in_remote_operations);
+        } else {
+            var oldid = localStorage.transid;
+            localStorage.transid = respObj.results[0].new_most_recent_operation_transaction_id;
+            debug("got new transid: changed from " + oldid + " to " + localStorage.transid);
+            callback(newuuid);
+        }
+
+    };
+    xhr.send(urlEncData);
+}
+
 
--- src/popup.js
+++ src/popup.js
@@ -3,17 +3,14 @@
 // available under MIT license
 
 
-function g(){return((1+Math.random())*65536|0).toString(16).substring(1)}
-function generateUUID(){return g()+g()+"-"+g()+"-"+g()+"-"+g()+"-"+g()+g()+g()}
-
 // send console messages to background page
-console = chrome.extension.getBackgroundPage().console;
+debug = chrome.extension.getBackgroundPage().debug;
 
 document.addEventListener('DOMContentLoaded', function() {
 
     chrome.tabs.query({ active: true, currentWindow: true }, function(tabs) {
         var tab = tabs[0];
-        console.log("browser action got clicked @ " + tab.url);
+        debug("browser action got clicked @ " + tab.url);
 
         $("body").on("click", "a", function() {
             chrome.tabs.create({url: $(this).attr("href") });
@@ -31,13 +28,13 @@ document.addEventListener('DOMContentLoaded', function() {
 });
 
 function initClipperPopup(tab) {
-    console.log("Trying to show clipper popup");
+    debug("Trying to show clipper popup");
 
     if (! localStorage.userid || ! localStorage.inbox) {
         showErrMsg("Extension not configured. " 
             + "<a href=\"https://workflowy.com\">Navigate " 
             + "to WorkFlowy</a> then press this button again.");
-        console.log("extension not configured");
+        debug("extension not configured");
         return;
     }
 
@@ -67,12 +64,31 @@ function initClipperPopup(tab) {
     });
 
     $("#clipbutton").on("click", function () {  
-        clipToWorkflowy( $("#title").val(), $("#comment").val() );
+        chrome.extension.getBackgroundPage().clipToWorkflowy( $("#title").val(), $("#comment").val(), function(newuuid,errmsg) {
+
+            if (!errmsg) {
+                showSuccessMsg("Successfully clipped!<br><br>"
+                    + "<small><a href=\"https://workflowy.com/#/"
+                    + newuuid.substr(24) + "\">view in WorkFlowy</a>"
+                    + "</small>");
+
+            } else if (errmsg.match(/logged/)) {
+                showErrMsg("WorkFlowy thinks you're logged out! Make sure you're logged in, and if this message persists then try resynchronizing the extension.");
+            } else if (errmsg.match(/results/)) {
+                showErrMsg("Malformed response from WorkFlowy, possible connection error.");
+            } else if (errmsg.match(/generic/)) {
+                showErrMsg("Error adding item, destination list may no longer exist.");
+            } else if (errmsg.match(/unknown/)) {
+                showErrMsg("Unknown error!");
+            } else {
+            }
+
+        });
     });
 }
 
 function initConfigPopup(tab) {
-    console.log("showing workflowy config popup");
+    debug("showing workflowy config popup");
     setPopupMode("config");
     
     var status;
@@ -87,11 +103,11 @@ function initConfigPopup(tab) {
     $("#status").html("Status: " + status);
 
     $("#inboxbutton").on("click", function() {
-        console.log("asking page for current project id");
+        debug("asking page for current project id");
         chrome.tabs.executeScript(
             { code: "document.getElementsByClassName(\"project selected\")[0].getAttribute(\"projectid\");" },
             function(response) {
-                console.log("page responded with " + response);
+                debug("page responded with " + response);
                 if (response && response != "None") {
                     localStorage.inbox = response;
                     showSuccessMsg("Clipped items will now appear in this list.");
@@ -115,83 +131,6 @@ function initConfigPopup(tab) {
 }
 
 
-function htmlEsc(str) {
-    return str.replace(/&/g,"&amp;").replace(/>/g,"&gt;").replace(/</g,"&lt;");
-}
-
-function clipToWorkflowy(title, comment) {
-    var newuuid = generateUUID();
-    var timestamp = Math.floor((new Date()).getTime()/1000) - localStorage.joined;
-    
-    var request = [{
-        "most_recent_operation_transaction_id": localStorage.transid,
-        "operations": [
-            { "type": "create",
-              "data": {
-                  "projectid": newuuid,
-                  "parentid":  localStorage.inbox,
-                  "priority":  9999
-               },
-              "client_timestamp": timestamp,
-              "undo_data":        {}
-            },
-            { "type":"edit",
-              "data": {
-                  "projectid":   newuuid,
-                  "name":        htmlEsc(title),
-                  "description": htmlEsc(comment)
-              },
-              "client_timestamp": timestamp,
-              "undo_data": {
-                   "previous_last_modified": timestamp,
-                   "previous_name":          "",
-                   "previous_description":   ""
-              }
-            }
-        ]
-    }];
-
-    console.log("Trying to clip something to workflowy:",
-        { timestamp: timestamp,
-          title: title,
-          comment: comment,
-          escapedTitle: htmlEsc(title),
-          escapedComment: htmlEsc(comment),
-          localstorage: localStorage }
-    );
-
-    var data = new FormData();
-    data.append("client_id",          "2015-11-17 19:25:15.397732");
-    data.append("client_version",     15);
-    data.append("push_poll_id",       localStorage.pollid);
-    data.append("push_poll_data",     JSON.stringify(request));
-    data.append("crosscheck_user_id", localStorage.userid);
-
-    var xhr = new XMLHttpRequest();
-    xhr.open('POST', 'https://workflowy.com/push_and_poll', true);
-    xhr.onload = function () {
-        var respObj = JSON.parse(this.responseText);
-        console.log("XHR response from server:", respObj);
-        
-        if (respObj.logged_out) {
-            showErrMsg("WorkFlowy thinks you're logged out! Make sure you're logged in, and if this message persists then try resynchronizing the extension.");
-        } else if ( ! respObj.results ) {
-            showErrMsg("Unknown error!");
-        } else if (respObj.results[0].error_encountered_in_remote_operations) {
-            showErrMsg("Unknown error!");
-        } else {
-            var oldid = localStorage.transid;
-            localStorage.transid = respObj.results[0].new_most_recent_operation_transaction_id;
-            console.log("got new transid: changed from " + oldid + " to " + localStorage.transid);
-            showSuccessMsg("Successfully clipped!<br><br>"
-                + "<small><a href=\"https://workflowy.com/#/"
-                + newuuid.substr(24) + "\">view in WorkFlowy</a>"
-                + "</small>");                    
-        }
-    };
-    xhr.send(data);
-    
-}
 
 function setPopupMode(mode) {
     $("body").removeClass("clip info config").addClass(mode);
